ccde81df30428c7b1a1ae0a460318027e9815ca8
Underset <21654428@qq.com>
2019-04-09 12:55:45 +0800

    -

---
+++ src/main/java/communal/Test.java
@@ -0,0 +1,11 @@
+package communal;
+
+import redis.clients.jedis.Jedis;
+
+public class Test {
+
+    public static void main(String args[]) {
+
+        Jedis jedis = new Jedis("", 6379);
+    }
+}
