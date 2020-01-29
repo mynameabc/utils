b3cb230ae60b78c330dfc00fdb62b07f3d178fc5
mynameabc <21654428@qq.com>
2020-01-30 00:01:04 +0800

    -

--- src/main/java/communal/Result.java
+++ src/main/java/communal/Result.java
@@ -11,22 +11,19 @@ import lombok.Data;
 public class Result {
 
 	private String code;
-	private boolean success;
 	private String message;
 	private Object data;
 
 	public Result() {
 
 		this.code = "0000";
-		this.success = true;
 		this.message = "";
 		this.data = null;
 	}
 
-	public Result(boolean success, String message) {
+	public Result(String message) {
 
 		this();
-		this.success = success;
 		this.message = message;
 	}
 
@@ -37,24 +34,15 @@ public class Result {
 		this.message = message;
 	}
 
-	public Result(boolean success, String message, Object data) {
+	public Result(String message, Object data) {
 
 		this();
-		this.success = success;
 		this.message = message;
 		this.data = data;
 	}
 
 	public Result(String code, String message, Object data) {
 		this();
-		this.message = message;
-		this.data = data;
-		this.code = code;
-	}
-
-	public Result(String code, boolean success, String message, Object data) {
-		this();
-		this.success = success;
 		this.message = message;
 		this.data = data;
 		this.code = code;
