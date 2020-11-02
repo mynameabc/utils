0bba40d845edc0cac5d11d42dff0e6d8e60d26d4
mynameabc <21654428@qq.com>
2020-11-02 15:25:34 +0800

    -

--- src/main/java/communal/util/RegexUtil.java
+++ src/main/java/communal/util/RegexUtil.java
@@ -1,5 +1,6 @@
 package communal.util;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -11,14 +12,14 @@ public class RegexUtil {
 
     /**
      * 用户名验证
-     * @param account
+     * @param name
      * @return
      * 4-16位字母, 数字, 下划线或中文
      */
-    public static boolean username(String account) {
+    public static boolean username(String name) {
 
         return Pattern.compile("^(?!\\d+$)(?=[a-zA-Z\\d_一-龥]*$).{6,18}$")
-                .matcher(account)
+                .matcher(name)
                 .matches();
     }
 
@@ -37,25 +38,25 @@ public class RegexUtil {
 
     /**
      * 手机号码验证
-     * @param mobileNumber
+     * @param mobile
      * @return
      */
-    public static boolean mobile(String mobileNumber) {
+    public static boolean isMobile(String mobile) {
 
-        return Pattern.compile("^1[3-9]\\d{9}$")
-                .matcher(mobileNumber)
+        return Pattern.compile("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$")
+                .matcher(mobile)
                 .matches();
     }
 
     /**
      * 邮箱地址验证
-     * @param EmailAddress
+     * @param email
      * @return
      */
-    public static boolean email(String EmailAddress) {
+    public static boolean isEmail(String email) {
 
         return Pattern.compile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?")
-                .matcher(EmailAddress)
+                .matcher(email)
                 .matches();
     }
 
@@ -64,14 +65,25 @@ public class RegexUtil {
      * @param money
      * @return
      */
-    public static boolean money(String money) {
+    public static boolean isMoney(String money) {
 
         return Pattern.compile("(^[1-9]([0-9]+)?(\\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\\.[0-9]([0-9])?$)")
                 .matcher(money)
                 .matches();
     }
 
-    public static boolean validator_IDCard(String idcard) {
+    private static boolean isDate(String strDate) {
+        Pattern pattern = Pattern
+                .compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
+        Matcher m = pattern.matcher(strDate);
+        if (m.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isIDCard(String idcard) {
 
         if (idcard.length() == 15) {
 
@@ -98,7 +110,7 @@ public class RegexUtil {
             System.out.println("不通过!");
         }
 
-        System.out.println(RegexUtil.mobile(""));
+        System.out.println(RegexUtil.isMobile(""));
 
         /*
 		System.out.println(RegexUtil.validator_Email("135@59193463.x"));
